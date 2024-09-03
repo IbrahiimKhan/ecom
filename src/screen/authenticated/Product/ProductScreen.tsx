bda447bc39fa51936abba88d118c7155ffc2cb02
Khan Ebrahim <150028854+iideviibrahim@users.noreply.github.com>
2024-09-03 12:24:09 +0600

    add vector icon libraray

--- src/screen/authenticated/Product/ProductScreen.tsx
+++ src/screen/authenticated/Product/ProductScreen.tsx
@@ -1,10 +1,12 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 
 const ProductScreen = () => {
   return (
     <View>
       <Text>ProductScreen</Text>
+      <AntDesign name="home" />
     </View>
   );
 };
